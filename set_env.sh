977ee0bcaf316bc0f938c4c0231d4f182bef2ef9
Tumeric <datps1234@gmail.com>
2023-10-05 20:24:20 +0700

    add set_env

---
+++ set_env.sh
@@ -0,0 +1,9 @@
+POSTGRES_USERNAME=eric
+POSTGRES_PASSWORD=12345678
+POSTGRES_HOST=tumericdat.crd5cgsawih6.us-east-1.rds.amazonaws.com
+POSTGRES_DB=postgres
+AWS_BUCKET=test-744726797992-dev
+AWS_REGION=us-east-1
+AWS_PROFILE=default
+JWT_SECRET=testing
+URL=a76ed6106460b49018eed63deea9c4a7-235055628.us-east-1.elb.amazonaws.com
